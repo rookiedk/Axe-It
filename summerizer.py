9fb3c71b8609fee43bebff5cdfb789a68dd28e26
Deepika Anandakrishnan <deepika.anandakrishnan@gmail.com>
2019-04-16 22:47:05 -0400

    summarizer with spaces

---
+++ summerizer.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+from gensim.summarization import summarize
+
+def read_plot(input):
+    title = input.split()
+    if len(title) > 1:
+        title = input.replace(' ', '_')
+    else:
+        title = input
+
+    url = 'https://en.wikipedia.org/wiki/{}'.format(title)
+    response = requests.get(url)
+    full_text = ''
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for header in soup.find_all('h2'):
+        if len(header.find_all('span', text='Plot')) != 0:
+            paragraphs = header.find_next_siblings('p', limit=10)
+            for p in paragraphs:
+                full_text += ('\n'+ p.text)
+    print('Complete Text: ' + '\n' + full_text +'\n\n')
+    return full_text
+input_text = read_plot('Black Panther (film)')
+print('Summarized Text: \n' + summarize(input_text))
